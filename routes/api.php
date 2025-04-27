0dbe2f9114c028ba6ed5f36f3f71717cb4a50ec6
Anvar <anv.sult@gmail.com>
2025-04-27 15:37:37 -0400

    Got everything working, missing: contrast, admin perm, disallow adding to cart when no stock, last payment page, form validation

--- routes/api.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace App\Http\Controllers;
-use App\Http\Controllers\ProductController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/products/search', [ProductController::class, 'search'])->name('products.search');
